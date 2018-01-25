c63230f9396339bb899040e99db74aa97e0e57f6
Colombianito66 <colombianito66@gmail.com>
2018-01-25 01:07:39 -0500

    add styles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
+import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-    <div> 
-      <div> Countdown to my BDAY!</div>
+    <div className='App'> 
+      <div className='App-title'> Countdown to my BDAY!</div>
         <div>
-        <div>14 days</div>
-        <div>30 hrs</div>
-        <div>40 minutes</div>
-        <div>12 seconds</div>
+        <div className='Clock-days'> 14 days</div>
+        <div className='Clock-hours'> 30 hrs</div>
+        <div className='Clock-minutes'> 40 minutes</div>
+        <div className='Clock-seconds'> 12 seconds</div>
         </div>
         <div>
           <input placeholder='new date' />
